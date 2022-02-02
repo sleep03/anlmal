9eb407410247e85a84e1ee66c093f0322897e228
sleep03 <76980865+sleep03@users.noreply.github.com>
2022-02-01 22:01:56 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+//https://teachablemachine.withgoogle.com/models/QpOZmfggo/
+
+
+function startClassification()
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/QpOZmfggo/model.json'
+    , modelReady); 
+}
+
+function modelReady(){
+    classifier.classify(gotResults);
+}
